782a88b5cb1721dfd5886059c2c37d012bcb43f8
Mingotrees <jhanell.mingo@gmail.com>
2025-06-20 16:34:55 +0800

    I'll work on finishing the log-in and register logic later

--- api/controllers/UserController.php
+++ api/controllers/UserController.php
@@ -17,4 +17,21 @@ class UserController{
         }
         
     }
+
+    public function register(){
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if(empty(trim($data['user_email'])) || empty(trim($data['user_password'])) || empty('')){
+            ErrorHelper::sendError(400, "Email and password are required");
+            return;
+        }
+
+        $user = new User();
+        $existingUser = $user->findByEmail($data['user_email']);
+
+        if($existingUser){
+            print_r($existingUser['user_email']);
+        }
+        
+    }
 }
