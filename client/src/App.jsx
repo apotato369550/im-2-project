3b844a79cf96bf877b59e08f0ce0272dd9cbf59e
Mingotrees <jhanell.mingo@gmail.com>
2025-07-05 16:08:39 +0800

    merge with john

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,8 @@
-import RegisterTestingSpace from './components/RegisterTestingSpace'
-import LoginTestingSpace from './components/LoginTestingSpace';
-import ProfileTestingSpace from './components/ProfileTestingSpace';
-import OrderCreationTestingSpace from './components/OrderCreationTestingSpace';
 
 const App = () => {
 
   return (
     <>
-      {/* <RegisterTestingSpace/> */}
-      {/* <LoginTestingSpace/> */}
-      {/* <ProfileTestingSpace/> */}
-      <OrderCreationTestingSpace/>
     </>
   );
 };
