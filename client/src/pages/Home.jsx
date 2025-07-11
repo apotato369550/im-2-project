4026a326380d81fb8972ebb0728281e3c92f8e37
Atheena Marie <22800058@usc.edu.ph>
2025-07-11 18:05:26 +0800

    done with login sign up i thenk

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Services from "../components/Services";
 import Brands from "../components/Brands";
 import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
-
 import React from "react"; 
 
 const Home = () => {
