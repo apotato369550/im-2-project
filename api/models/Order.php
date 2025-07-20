96be055c3f4b70a37d934e8d4c5f4a09cd181f45
Mingotrees <jhanell.mingo@gmail.com>
2025-07-20 14:39:20 +0800

    clientDashboard connected to backend

--- api/models/Order.php
+++ api/models/Order.php
@@ -12,7 +12,7 @@ class Order{
                 o.order_date_created,
                 u.user_id,
                 u.user_full_name,
-                u.user_email,
+                u.user_email, 
                 s.service_id,
                 s.service_type, 
                 s.service_details, 
@@ -37,9 +37,19 @@ class Order{
     {
         $db = DBHelper::getConnection();
         $stmt = $db->prepare(
-            "SELECT * 
-            FROM orders
-            WHERE client_id = :userId");
+            "SELECT 
+                o.*,
+                s.*,
+                q.quotation_id,
+                q.total_payment,
+                -- Don't select q.order_id since it conflicts
+                a.assignment_id
+            FROM orders o 
+            LEFT JOIN quotation q ON q.order_id = o.order_id
+            LEFT JOIN assignments a ON a.order_id = o.order_id
+            LEFT JOIN service s ON o.service_id = s.service_id
+            WHERE o.client_id = :userId
+            GROUP BY o.order_id");
         $stmt->execute(['userId' => $userId]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC) ?: null;
     }
